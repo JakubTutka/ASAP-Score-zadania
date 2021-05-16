7b0466b72bd1a94ffabc740751a0bfab5ee9db37
JakubTutka <56030685+JakubTutka@users.noreply.github.com>
2021-05-16 22:37:28 +0200

    Create zadanie1.py

---
+++ zadanie1.py
@@ -0,0 +1,17 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        # ------ TO DO ------
+
+
+class MyLoginApp(App):
+    def build(self):
+        return MyGrid()
+
+MyLoginApp().run()
